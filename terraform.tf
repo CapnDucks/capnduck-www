2916e5da651cb642a44ba8b6702fde7db8aa4f10
Cap'n Duck <coder@capnduck.com>
2024-03-12 08:39:16 -0700

    Adding remote backend.

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket         = "capnduck-terraform-tfstate"
+    key            = "capnduck-www.tfstate"
+    dynamodb_table = "terraform-tfstate-lock"
+    region         = "us-west-2"
+    encrypt        = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
