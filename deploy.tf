2bf449ed12164f957b0f5d29adb7395651c755bd
jelliott.io <jelliott.io@capnduck.com>
2024-03-21 14:39:54 -0700

    Updating html files and adding apex domain routing.

--- deploy.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "deploy_www" {
-  provisioner "local-exec" {
-    command = "aws s3 sync html/ s3://${aws_s3_bucket.www.id}"
-  }
-
-  depends_on = [
-    aws_s3_bucket.www
-  ]
-}
