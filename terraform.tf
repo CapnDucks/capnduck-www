0cc0052120006fb957809df71404d7a34e5bcb0a
jelliott.io <jelliott.io@capnduck.com>
2025-04-16 11:48:33 -0700

    Terraform fmt.

--- terraform.tf
+++ terraform.tf
@@ -2,11 +2,11 @@ terraform {
   required_version = "= 1.11.4"
 
   backend "s3" {
-    bucket         = "capnduck-terraform-tfstate"
-    encrypt        = true
-    key            = "capnduck-www.tfstate"
-    region         = "us-west-2"
-    use_lockfile   = true
+    bucket       = "capnduck-terraform-tfstate"
+    encrypt      = true
+    key          = "capnduck-www.tfstate"
+    region       = "us-west-2"
+    use_lockfile = true
   }
 
   required_providers {
